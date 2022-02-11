ccd7b0423005f5ebaa0265fab786d83db8509fa7
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-02-11 11:21:50 -0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,57 @@
-import br.com.nbs.desafio.dominio.Curso;
-import br.com.nbs.desafio.dominio.Mentoria;
+import br.com.nbs.desafio.dominio.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
         Curso curso = new Curso();
         Mentoria mentoria= new Mentoria();
 
         curso.setTitulo("POO");
         curso.setDescricao("Curso de Paradigma Programação Orientada a Objeto");
         curso.setCargaHoraria(2);
-        System.out.println(curso);
+//        System.out.println(curso);
+
 
         mentoria.setDataInicial(LocalDate.now());
         mentoria.setTitulo("Java-POO");
         mentoria.setDescricao("Mentoria sobre POO em java");
-        System.out.println(mentoria);
+//        System.out.println(mentoria);
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("Bootcamp Java GFT");
+        bootCamp.setDescricao("Curso Basico Java POO");
+        bootCamp.getConteudos().add(curso);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devjo = new Dev();
+        devjo.setNome("Dev João");
+        devjo.inscreverBootcamp(bootCamp);
+        System.out.println("Conteúdos Inscritos Joao " + devjo.getConteudosInscritos());
+        devjo.progredir();
+        devjo.progredir();
+        System.out.println("Conteúdos Concluidos Joao " + devjo.getConteudosConcluidos());
+        System.out.println("XP" + devjo.calcularXp());
+
+        System.out.println("-------");
+
+        Dev devCami = new Dev();
+        devCami.setNome("Dev Cami");
+        devCami.inscreverBootcamp(bootCamp);
+        System.out.println("Conteúdos Inscritos Cami " + devCami.getConteudosInscritos());
+        devCami.progredir();
+        System.out.println("Conteúdos Concluidos Joao " + devCami.getConteudosConcluidos());
+        System.out.println("XP" + devCami.calcularXp());
+
+
+
+
+
+
+
 
     }
 }
